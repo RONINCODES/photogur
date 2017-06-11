6232cdfb6c4f660d06455cb6b02f69dd6651c711
Ronen Annason <ronenannason83@gmail.com>
2017-06-11 00:41:05 -0400

    Photogur complete - will re-do for practice

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  get 'pictures/index'
-
-get 'pictures' => 'pictures#index'  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'pictures#index'
+  get 'pictures' => 'pictures#index'  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post 'pictures' => 'pictures#create'
+  get 'pictures/new' => 'pictures#new'
+  get 'pictures/:id/edit' => "pictures#edit"
+  patch 'pictures/:id' => "pictures#update"
+  get 'pictures/:id' => 'pictures#show'
+  delete 'pictures/:id' => 'pictures#destroy'
 end
