0b7e808bdeeb4ce2da904ce464d9b68ea5a45366
Ronen Annason <ronenannason83@gmail.com>
2017-06-14 20:00:34 -0400

    added validations to picture model

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,5 +1,12 @@
 class Picture < ApplicationRecord
 
+  validates :artist, presence: true
+  validates :title, presence: true
+  validates :url, presence: true
+  validates :url, uniqueness: true
+  validates :url, format: {with: /https?:\/\/[\S]+/} 
+
+
   def self.created_before(time)
     Picture.where("created_at < ?", time)
   end
